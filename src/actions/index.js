f67617035a7b011e1c9fd1863c98e3bf96a1f4a3
Andrew Sindle-Mouat <andy@asindlemouat.co.uk>
2018-09-08 07:52:43 +0100

    Update API Code

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'cbdc21ebde252dc7b6104a9c51808b66';
+const API_KEY = '6ad503281e72570015f6f49c044fa85f';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
